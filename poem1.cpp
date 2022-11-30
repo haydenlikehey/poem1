88313e4afbf4a8232c0e9371c2ef710dc47b5618
Heyden <hgrace0001@gmail.com>
2022-11-30 17:43:15 -0600

    Version 1

---
+++ poem1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+
+ cout << "I'm worried that all the art I want to make will come off as pretentious.\n\n";
+ cout << "For example, why was there a newline character placed between this line and the last?\n\n\n";
+ cout << "And look! Now there were two between this one!\n\n";
+ cout << "What does it even mean? Am I gonna have to write explanations like TS Elliot or\nwhatever?\n\n";
+ cout << "I don't want to do that, that sounds awful and pretentious.\n\n";
+ cout << "I learned what a newline character from trying to program.\n\n";
+ cout << "Actually, that sounds like a good idea. What if I wrote a poem that was a program?\n\n";
+ cout << "Now I'm actually doing it.\n\n";
+ cout << "Before I thought, \"That would be a good idea for a poem!\" and now I'm writing it in Atom.";
+ cout << "Wait, but that seems like it might be pretentious too...\n\n";
+ cout << "And look what can happen.\n\n";
+ cout << "Those two lines before last merged becuase I didn't use \\n or << endl...\n\n";
+ cout << "Now that I think about it that seems pretty pretentious on the surface.\n\n";
+ cout << "Let alone what hosting a poem on github would look like.\n\n";
+ cout << "You know, I think I'll just leave it there.\n\n";
+ cout << "Quitting feels like the least pretentious thing I can do";
+
+}
